7367a188d5ece3c1a8682f26f4449a19a4564d4b
Luerfel <mluerfel@gmail.com>
2024-11-03 18:49:44 -0300

    definação das estruturas

--- Pilha.cpp
+++ Pilha.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+typedef struct dados{
+    char nome[50];
+    int idade;
+}dados;
+
+typedef struct elemento{
+    elem* ant;
+    dados cadastro;
+}elem;
+
+typedef struct descritor{
+    elem* ultimo;
+    int tamanho;
+}descritor;
 // o último elemento inserido é o primeiro a ser removido.
 int main(){
 
